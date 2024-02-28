427288f255d9358c77ad428d47d2f29fdb46dc6c
Bryankesler <55001654+Bryankesler@users.noreply.github.com>
2024-02-28 10:34:01 -0500

    new 3.5 model

--- src/routes/api/chat/+server.ts
+++ src/routes/api/chat/+server.ts
@@ -66,7 +66,7 @@ export const POST: RequestHandler = async ({ request }) => {
 		]
 
 		const chatRequestOpts: CreateChatCompletionRequest = {
-			model: 'gpt-3.5-turbo-0613',
+			model: 'gpt-3.5-turbo',
 			messages,
 			temperature: 0.9,
 			stream: true
